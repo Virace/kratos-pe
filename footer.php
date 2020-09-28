abc30a19ebf8774b53f0000f0ff58ea653494509
Virace <Virace@aliyun.com>
2020-09-29 04:55:13 +0800

    修复pjax二次进入文章时灯箱失效问题

--- footer.php
+++ footer.php
@@ -53,14 +53,15 @@
                 if (kratos_option('s_gov')) {
                     echo '<p><a href="' . kratos_option('s_gov_link', '#') . '" target="_blank" rel="nofollow" ><i class="police-ico"></i>' . kratos_option('s_gov') . '</a></p>';
                 }
-                if (kratos_option('seo_statistical')) {
-                    echo '<p>' . kratos_option('seo_statistical') . '</p>';
-                }
                 ?>
             </div>
         </div>
     </div>
 </footer>
-<?php wp_footer(); ?>
+<?php
+if (kratos_option('seo_statistical')) {
+    echo kratos_option('seo_statistical');
+}
+wp_footer(); ?>
 </body>
 </html>
