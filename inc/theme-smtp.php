16328f507f87ba4b93c9e388d9414cdd25055044
Virace <Virace@aliyun.com>
2020-11-30 04:34:14 +0800

    更换邮件模板, 解决默认模板被长用邮件服务提供商拦截垃圾邮件问题.

--- inc/theme-smtp.php
+++ inc/theme-smtp.php
@@ -19,6 +19,7 @@ if (kratos_option('m_smtp', false)) {
         $phpmailer->Username = kratos_option('m_username');
         $phpmailer->Password = kratos_option('m_passwd');
     }
+
     add_action('phpmailer_init', 'mail_smtp');
 }
 
@@ -27,6 +28,7 @@ function wp_mail_debug($wp_error)
 {
     return error_log(print_r($wp_error, true));
 }
+
 // add_action('wp_mail_failed', 'wp_mail_debug', 10, 1);
 
 function comment_approved($comment)
@@ -36,45 +38,19 @@ function comment_approved($comment)
         $to = trim($comment->comment_author_email);
         $post_link = get_permalink($comment->comment_post_ID);
         $subject = __('[通知]您的留言已经通过审核', 'kratos');
-        $message = '
-            <div style="background:#ececec;width: 100%;padding: 50px 0;text-align:center;">
-            <div style="background:#fff;width:750px;text-align:left;position:relative;margin:0 auto;font-size:14px;line-height:1.5;">
-                    <div style="zoom:1;padding:25px 40px;background:#518bcb; border-bottom:1px solid #467ec3;">
-                        <h1 style="color:#fff; font-size:25px;line-height:30px; margin:0;"><a href="' . get_option('home') . '" style="text-decoration: none;color: #FFF;">' . htmlspecialchars_decode(get_option('blogname'), ENT_QUOTES) . '</a></h1>
-                    </div>
-                <div style="padding:35px 40px 30px;">
-                    <h2 style="font-size:18px;margin:5px 0;">' . __('您好，', 'kratos') . trim($comment->comment_author) . '：</h2>
-                    <p style="color:#313131;line-height:20px;font-size:15px;margin:20px 0;">' . __('您的留言已经通过了管理员的审核，摘要信息如下：', 'kratos') . '</p>
-                        <table cellspacing="0" style="font-size:14px;text-align:center;border:1px solid #ccc;table-layout:fixed;width:500px;">
-                            <thead>
-                                <tr>
-                                    <th style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:normal;color:#a0a0a0;background:#eee;border-color:#dfdfdf;" width="280px;">' . __('文章', 'kratos') . '</th>
-                                    <th style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:normal;color:#a0a0a0;background:#eee;border-color:#dfdfdf;" width="270px;">' . __('内容', 'kratos') . '</th>
-                                    <th style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:normal;color:#a0a0a0;background:#eee;border-color:#dfdfdf;" width="110px;">' . __('操作', 'kratos') . '</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">《' . get_the_title($comment->comment_post_ID) . '》</td>
-                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">' . trim($comment->comment_content) . '</td>
-                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;"><a href="' . get_comment_link($comment->comment_ID) . '" style="color:#1E5494;text-decoration:none;vertical-align:middle;" target="_blank">查看留言</a></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <br>
-                    <div style="font-size:13px;color:#a0a0a0;padding-top:10px">' . __('该邮件由系统自动发出，如果不是您本人操作，请忽略此邮件。', 'kratos') . '</div>
-                    <div class="qmSysSign" style="padding-top:20px;font-size:12px;color:#a0a0a0;">
-                        <p style="color:#a0a0a0;line-height:18px;font-size:12px;margin:5px 0;">' . htmlspecialchars_decode(get_option('blogname'), ENT_QUOTES) . '</p>
-                        <p style="color:#a0a0a0;line-height:18px;font-size:12px;margin:5px 0;"><span style="border-bottom:1px dashed #ccc;" t="5" times="">' . date("Y年m月d日", time()) . '</span></p>
-                    </div>
-                </div>
-            </div>
-        </div>';
+
+        $body = __('您在', 'kratos') . '《<a href="' . $post_link . '" target="_blank" >' . get_the_title($comment->comment_post_ID) . '</a>》' . __('中发表的评论已通过审核！', 'kratos') . '<br /><br />';
+        $body .= '<strong>' . __('您的评论', 'kratos') . ':</strong><br />';
+        $body .= strip_tags($comment->comment_content) . '<br /><br />';
+        $body .= __('您可以', 'kratos') . ':<a href="' . get_comment_link($comment->comment_ID) . '" target="_blank">' . __('查看您的评论', 'kratos') . '</a>  |  <a href="' . $post_link . '#comments" target="_blank">' . __('查看其他评论', 'kratos') . '</a>  |  <a href="' . $post_link . '" target="_blank">' . __('再次阅读文章', 'kratos') . '</a><br /><br />';
+        $body .= __('欢迎再次光临', 'c') . '【<a href="' . get_bloginfo('url') . '" target="_blank" title="' . get_bloginfo('description') . '">' . get_bloginfo('name') . '</a>】。';
+        $body .= __('该邮件由系统自动发出，如果不是您本人操作，请忽略此邮件。', 'kratos');
         $from = "From: \"" . htmlspecialchars_decode(get_option('blogname'), ENT_QUOTES) . "\" <$wp_email>";
         $headers = "$from\nContent-Type: text/html; charset=" . get_option('blog_charset') . "\n";
-        wp_mail($to, $subject, $message, $headers);
+        wp_mail($to, $subject, $body, $headers);
     }
 }
+
 add_action('comment_unapproved_to_approved', 'comment_approved');
 
 function comment_notify($comment_id)
@@ -86,45 +62,42 @@ function comment_notify($comment_id)
         $wp_email = kratos_option('m_username');
         $to = trim(get_comment($parent_id)->comment_author_email);
         $subject = __('[通知]您的留言有了新的回复', 'kratos');
-        $message = '
-            <div style="background:#ececec;width: 100%;padding: 50px 0;text-align:center;">
-            <div style="background:#fff;width:750px;text-align:left;position:relative;margin:0 auto;font-size:14px;line-height:1.5;">
-                    <div style="zoom:1;padding:25px 40px;background:#518bcb; border-bottom:1px solid #467ec3;">
-                        <h1 style="color:#fff; font-size:25px;line-height:30px; margin:0;"><a href="' . get_option('home') . '" style="text-decoration: none;color: #FFF;">' . htmlspecialchars_decode(get_option('blogname'), ENT_QUOTES) . '</a></h1>
-                    </div>
-                <div style="padding:35px 40px 30px;">
-                    <h2 style="font-size:18px;margin:5px 0;">' . __('您好，', 'kratos') . trim(get_comment($parent_id)->comment_author) . '：</h2>
-                    <p style="color:#313131;line-height:20px;font-size:15px;margin:20px 0;">' . __('您的留言有了新的回复，摘要信息如下：', 'kratos') . '</p>
-                        <table cellspacing="0" style="font-size:14px;text-align:center;border:1px solid #ccc;table-layout:fixed;width:500px;">
-                            <thead>
-                                <tr>
-                                    <th style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:normal;color:#a0a0a0;background:#eee;border-color:#dfdfdf;" width="235px;">' . __('原文', 'kratos') . '</th>
-                                    <th style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:normal;color:#a0a0a0;background:#eee;border-color:#dfdfdf;" width="235px;">' . __('回复', 'kratos') . '</th>
-                                    <th style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:normal;color:#a0a0a0;background:#eee;border-color:#dfdfdf;" width="100px;">' . __('作者', 'kratos') . '</th>
-                                    <th style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:normal;color:#a0a0a0;background:#eee;border-color:#dfdfdf;" width="90px;" >' . __('操作', 'kratos') . '</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">' . trim(get_comment($parent_id)->comment_content) . '</td>
-                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">' . trim($comment->comment_content) . '</td>
-                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;">' . trim($comment->comment_author) . '</td>
-                                    <td style="padding:5px 0;text-indent:8px;border:1px solid #eee;border-width:0 1px 1px 0;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;"><a href="' . get_comment_link($comment->comment_ID) . '" style="color:#1E5494;text-decoration:none;vertical-align:middle;" target="_blank">' . __('查看回复', 'kratos') . '</a></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <br>
-                    <div style="font-size:13px;color:#a0a0a0;padding-top:10px">' . __('该邮件由系统自动发出，如果不是您本人操作，请忽略此邮件。', 'kratos') . '</div>
-                    <div class="qmSysSign" style="padding-top:20px;font-size:12px;color:#a0a0a0;">
-                        <p style="color:#a0a0a0;line-height:18px;font-size:12px;margin:5px 0;">' . htmlspecialchars_decode(get_option('blogname'), ENT_QUOTES) . '</p>
-                        <p style="color:#a0a0a0;line-height:18px;font-size:12px;margin:5px 0;"><span style="border-bottom:1px dashed #ccc;" t="5" times="">' . date("Y年m月d日", time()) . '</span></p>
-                    </div>
-                </div>
-            </div>
-        </div>';
+
+        $message = '<table border="1" cellpadding="0" cellspacing="0" width="600" align="center" style="border-collapse: collapse; border-style: solid; border-width: 1;border-color:#ddd;">
+	<tbody>
+          <tr>
+            <td>
+				<table align="center" border="0" cellpadding="0" cellspacing="0" width="600" height="48" >
+                    <tbody><tr>
+                        <td width="100" align="center" style="border-right:1px solid #ddd;">
+                            <a href="' . home_url() . '/" target="_blank">' . get_option("blogname") . '</a></td>
+                        <td width="300" style="padding-left:20px;"><strong>' . __('您有一条来自', 'kratos') . ' <a href="' . home_url() . '" target="_blank" style="color:#F37474;text-decoration:none;">' . get_option("blogname") . '</a> ' . __('的回复', 'kratos') . '</strong></td>
+						</tr>
+					</tbody>
+				</table>
+			</td>
+          </tr>
+          <tr>
+            <td  style="padding:15px;">
+			<h1 style="font-weight: normal; color: #fff; text-align: center; margin-bottom: 65px;font-size: 20px; letter-spacing: 6px;font-weight: normal ; padding: 15px; background: #EA5A5A;">THANKS FOR YOUR REPLY.</h1>
+			<p><strong>' . trim(get_comment($parent_id)->comment_author) . '</strong>, ' . __('你好!', 'kratos') . '</span>
+              <p>' . __('你在', 'kratos') . '《' . get_the_title($comment->comment_post_ID) . '》' . __('的留言', 'kratos') . ':</p><p style="border-left:3px solid #ddd;padding-left:1rem;color:#999;">'
+            . trim(get_comment($parent_id)->comment_content) . '</p><p>
+              ' . trim($comment->comment_author) . ' ' . __('给你的回复', 'kratos') . ':</p><p style="border-left:3px solid #ddd;padding-left:1rem;color:#999;">'
+            . trim($comment->comment_content) . '</p>
+        <center style="padding:40px 0" ><a href="' . htmlspecialchars(get_comment_link($parent_id)) . '" target="_blank" style="background-color:#474A58; border-radius:0px; display:inline-block; color:#fff; padding:15px 20px 15px 20px; text-decoration:none;margin-top:20px; margin-bottom:20px;">' . __('点击查看完整内容', 'kratos') . '</a></center>
+</td>
+          </tr>
+          <tr>
+            <td align="center" valign="center" height="60" style="font-size:0.8rem; color:#999;">Copyright © ' . htmlspecialchars_decode(get_option('blogname'), ENT_QUOTES) . '</td>
+            <td align="center" valign="center" height="60" style="font-size:0.8rem; color:#999;">' . date("Y年m月d日", time()) . '</td>
+          </tr>
+		  </tbody>
+  </table>';
         $from = "From: \"" . htmlspecialchars_decode(get_option('blogname'), ENT_QUOTES) . "\" <$wp_email>";
         $headers = "$from\nContent-Type: text/html; charset=" . get_option('blog_charset') . "\n";
         wp_mail($to, $subject, $message, $headers);
     }
 }
+
 add_action('comment_post', 'comment_notify');
