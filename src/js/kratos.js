6382db58bbba882359bb9d5bb1a75aaffb832844
Virace <Virace@aliyun.com>
2020-11-30 04:36:30 +0800

    1.0.2

--- src/js/kratos.js
+++ src/js/kratos.js
@@ -4,7 +4,7 @@
  */
 (function () {
   'use strict'
-  const KRATOS_VERSION = '1.0.1'
+  const KRATOS_VERSION = '1.0.2'
 
   const navbarConfig = function () {
     const $menu = $('.dropdown-menu')
